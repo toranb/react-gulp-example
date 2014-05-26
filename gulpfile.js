0df0829252f759d752ec6bf63bbf528e983c0e76
toran.billups <toranb@gmail.com>
2014-05-26 11:57:20 -0500

    added dist step w/ concat

--- gulpfile.js
+++ gulpfile.js
@@ -1,14 +1,17 @@
 var gulp = require('gulp');
+var concat = require('gulp-concat');
 var react = require('gulp-react');
 
 var paths = {
-    jsx: [
+    deps: [
+        'bower_components/react/react.js',
         'src/**/*.jsx'
     ]
 };
 
 gulp.task('default', function () {
-    return gulp.src(paths.jsx)
+    return gulp.src(paths.deps)
         .pipe(react())
-        .pipe(gulp.dest('build'));
+        .pipe(concat('deps.min.js'))
+        .pipe(gulp.dest('dist'));
 });
