675375727a67b920abdbdd31701f42c2d2903516
toran billups <toranb@gmail.com>
2014-06-22 14:12:15 -0500

    karma 0.12.16 now works with chrome

--- karma.conf.js
+++ karma.conf.js
@@ -4,13 +4,9 @@ module.exports = function(karma) {
         files: [
           "dist/karma.min.js"
         ],
-        port: 9876,
-        runnerPort: 9100,
-        colors: true,
         reporters: ['progress'],
-        logLevel: karma.LOG_ERROR,
         browsers: ['Chrome'],
         singleRun: false,
-        autoWatch: true,
+        autoWatch: true
     });
 };
